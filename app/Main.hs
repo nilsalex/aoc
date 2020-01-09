bdfbf4d8ccb040d022b2bcba6cef6786995df7b8
Nils Alex <nils@famalex.de>
2020-01-09 13:34:28 +0100

    usage message

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,7 @@ main = do
         case args of
           []      -> a01_to_a24
           "25":[] -> a25
-          _       -> putStrLn "Usage: aoc-exe [25]\n\n\
+          _       -> putStrLn "Usage: aoc2019-exe [25]\n\n\
                                \Solves the puzzles from https://adventofcode.com/2019\n\
                                \Providing 25 as argument runs the game server for day 25 on port 3000. (connect via telnet localhost 3000)\n\
                                \Otherwise, days 1 to 24 are solved."
